bbe37474fa3fa60050e0d406cbb232b2a2c174b2
antony-jr <antonyjr@protonmail.com>
2023-12-10 04:03:56 +0530

    use correct paths

--- source/qt5_15_2/windows/clean.sh
+++ source/qt5_15_2/windows/clean.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-rm -rf packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019
-mkdir packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019
-cp packages/in.antonyjr.QArchive.windows.x64.msvc2019/meta/license.txt packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019/QARCHIVE.COPYING
+rm -rf packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64
+mkdir packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64
+cp packages/in.antonyjr.QArchive.windows.x64.msvc2019/meta/license.txt packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64/QARCHIVE.COPYING
 
-rm -rf packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019
-mkdir packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019
-cp packages/in.antonyjr.libarchive.windows.x64.msvc2019/meta/license.txt packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019/LIBARCHIVE.COPYING
+rm -rf packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019_64
+mkdir packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019_64
+cp packages/in.antonyjr.libarchive.windows.x64.msvc2019/meta/license.txt packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019_64/LIBARCHIVE.COPYING
 
 
