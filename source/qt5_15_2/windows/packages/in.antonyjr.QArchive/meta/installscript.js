4b897d7a279747d0acb68de1840778a42817c2e8
antony-jr <antonyjr@protonmail.com>
2023-12-10 13:02:58 +0530

    add license and try to install to the right qtdir.

--- source/qt5_15_2/windows/packages/in.antonyjr.QArchive/meta/installscript.js
+++ source/qt5_15_2/windows/packages/in.antonyjr.QArchive/meta/installscript.js
@@ -1,4 +1,13 @@
 function Component() {
+    var qtVersion = qtversion();
+
+    // Log the Qt version to the installer log
+    installer.setValue("QtVersion", qtVersion);
+    console.log("Qt version: " + qtVersion);
+    var path = "@RootDir@/" + qtVersion;
+
+    console.log("Path: " + path);
+
     /*
     if ( systemInfo.currentCpuArchitecture === "i386") {
         installer.componentByName("in.antonyjr.QArchive.windows.x86.msvc2019").setValue("Virtual", "false");
@@ -10,6 +19,9 @@ function Component() {
         installer.componentByName("in.antonyjr.QArchive.windows.x64.msvc2019").setValue("Virtual", "false");
         //installer.componentByName("in.antonyjr.QArchive.windows.x64.msvc2015").setValue("Virtual", "false"); 
         installer.componentByName("in.antonyjr.QArchive.windows.x64.msvc2019").setValue("Default", "true");
+
+        installer.componentByName("in.antonyjr.QArchive.windows.x64.msvc2019").setValue("TargetDir", path);
+        installer.componentByName("in.antonyjr.libarchive.windows.x64.msvc2019").setValue("TargetDir", path);
     }
 }
 
