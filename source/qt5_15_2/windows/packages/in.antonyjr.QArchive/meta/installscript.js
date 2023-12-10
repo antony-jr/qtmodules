08ea383302b0b64290669e776e62a644f52ad9d3
Antony Jr <33930174+antony-jr@users.noreply.github.com>
2023-12-10 14:25:37 +0530

    Try finding the Qt version

--- source/qt5_15_2/windows/packages/in.antonyjr.QArchive/meta/installscript.js
+++ source/qt5_15_2/windows/packages/in.antonyjr.QArchive/meta/installscript.js
@@ -4,7 +4,14 @@ function Component() {
     // Log the Qt version to the installer log
     //installer.setValue("QtVersion", qtVersion);
     //console.log("Qt version: " + qtVersion);
-    var path = "@RootDir@\\5.15.*";
+    var files = QDesktopServices.findFiles(".", "5.15.*");
+    var path = "@RootDir@\\";
+
+    if (files.length == 1) {
+        path = path + files[0];
+    } else {
+        path = path + "QArchive";
+    }
 
     console.log("Path: " + path);
 
@@ -20,6 +27,7 @@ function Component() {
         //installer.componentByName("in.antonyjr.QArchive.windows.x64.msvc2015").setValue("Virtual", "false"); 
         installer.componentByName("in.antonyjr.QArchive.windows.x64.msvc2019").setValue("Default", "true");
 
+        installer.componentByName("in.antonyjr.QArchive").setValue("TargetDir", path);
         installer.componentByName("in.antonyjr.QArchive.windows.x64.msvc2019").setValue("TargetDir", path);
         installer.componentByName("in.antonyjr.libarchive.windows.x64.msvc2019").setValue("TargetDir", path);
     }
