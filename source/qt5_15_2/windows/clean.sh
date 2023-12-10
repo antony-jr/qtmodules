bb6843118b362e9cfbf3182ce2f5e691e0c40e97
antony-jr <antonyjr@protonmail.com>
2023-12-10 22:43:38 +0530

    fix make scripts

--- source/qt5_15_2/windows/clean.sh
+++ source/qt5_15_2/windows/clean.sh
@@ -2,10 +2,10 @@
 
 rm -rf packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64
 mkdir packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64
-cp packages/in.antonyjr.QArchive.windows.x64.msvc2019/meta/license.txt packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64/QARCHIVE.COPYING
+cp packages/in.antonyjr.QArchive.windows.x64.msvc2019/meta/QARCHIVE_LICENSE.txt packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64/QARCHIVE.COPYING
 
 rm -rf packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019_64
 mkdir packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019_64
-cp packages/in.antonyjr.libarchive.windows.x64.msvc2019/meta/license.txt packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019_64/LIBARCHIVE.COPYING
+cp packages/in.antonyjr.libarchive.windows.x64.msvc2019/meta/LIBARCHIVE_LICENSE.txt packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019_64/LIBARCHIVE.COPYING
 
 
