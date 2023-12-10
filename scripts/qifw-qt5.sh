5365c88f0feefb1e43101629a637bc84193a9e69
antony-jr <antonyjr@protonmail.com>
2023-12-10 13:22:10 +0530

    try installing it to the correct dir.

--- scripts/qifw-qt5.sh
+++ scripts/qifw-qt5.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir qifw
-wget -O installer.bin "https://download.qt.io/official_releases/qt-installer-framework/4.5.2/QtInstallerFramework-linux-x64-4.5.2.run"
+curl --retry 12 --retry-delay 1 --max-time 10 --retry-all-errors -L -o installer.bin "https://download.qt.io/official_releases/qt-installer-framework/4.5.2/QtInstallerFramework-linux-x64-4.5.2.run"
 
 chmod +x installer.bin
-
-ldd installer.bin
 
 ./installer.bin in -t "$PWD/qifw" --am -c --al
 
