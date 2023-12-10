fd4f0b87ef37e5c0f7383b8616f0aef136a9ed35
antony-jr <antonyjr@protonmail.com>
2023-12-10 22:46:31 +0530

    fix clean.sh

--- source/qt5_15_2/windows/clean.sh
+++ source/qt5_15_2/windows/clean.sh
@@ -2,7 +2,7 @@
 
 rm -rf packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64
 mkdir packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64
-cp packages/in.antonyjr.QArchive.windows.x64.msvc2019/meta/QARCHIVE_LICENSE.txt packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64/QARCHIVE.COPYING
+cp packages/in.antonyjr.QArchive/meta/QARCHIVE_LICENSE.txt packages/in.antonyjr.QArchive.windows.x64.msvc2019/data/msvc2019_64/QARCHIVE.COPYING
 
 rm -rf packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019_64
 mkdir packages/in.antonyjr.libarchive.windows.x64.msvc2019/data/msvc2019_64
